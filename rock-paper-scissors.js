54eb0abe0b491d6c682fd1cadeec7897397c5679
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-09 16:27:29 +0200

    Add human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,3 +4,14 @@ function getComputerChoice() {
     console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
 getComputerChoice();
+
+// Human choice
+let choice = prompt("Write 'rock', 'paper' or 'scissors'");
+let humanChoice = choice.toLowerCase();
+if (arr.includes(humanChoice)) {
+    console.log(humanChoice);
+} else {
+    alert("It's not included!")
+}
+
+
