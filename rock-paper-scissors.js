dfd1169d32bcdb359d2727f766a55368540e1f39
Patricia Tirolla <patricia.tirolla@hyperisland.se>
2024-09-09 18:12:32 +0200

    Add play round and fixes human choice
    Creates function humanChoice to wrap the if-else
    
    Adds "try again" logic and fixes return function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,45 @@
 // Computer random choice
-let arr = ["rock", "paper", "scissors"]
+const arr = ["rock", "paper", "scissors"]
 function getComputerChoice() {
-    console.log(arr[Math.floor(Math.random() * arr.length)]);
+    const choice = arr[Math.floor(Math.random() * arr.length)];
+    console.log(choice);
+    return choice;
 }
-getComputerChoice();
 
 // Human choice
-let choice = prompt("Write 'rock', 'paper' or 'scissors'");
-let humanChoice = choice.toLowerCase();
-if (arr.includes(humanChoice)) {
-    console.log(humanChoice);
-} else {
-    alert("It's not included!")
+function getHumanChoice() {
+    let choice = prompt("Write 'rock', 'paper' or 'scissors'");
+    let humanChoice = choice.toLowerCase();
+    if (arr.includes(humanChoice)) {
+        console.log(humanChoice);
+    } else {
+        alert("It's not included!")
+        return getHumanChoice();
+    }
+    return humanChoice;
 }
 
+// players variables
+let humanScore = 0;
+let computerScore = 0;
+
+// conditions
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice == "rock" && humanChoice == "paper") {
+        humanScore = humanScore + 1;
+        console.log("You win!");
+    } else if (computerChoice == "paper" && humanChoice == "scissors") {
+        humanScore = humanScore + 1;
+        console.log("You win!");
+    } else if (computerChoice == "scissors" && humanChoice == "rock") {
+        humanScore = humanScore + 1;
+        console.log("You win!");
+    } else if (computerChoice == humanChoice) {
+        console.log("Tie. Try again!");
+    } else {
+        computerScore = computerScore + 1;
+        console.log("You lose!");
+    }
+}
+playRound(getHumanChoice(), getComputerChoice());
 
